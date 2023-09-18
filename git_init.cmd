8a9d68bed6f926486e0575738047540033288142
kot-omka <kot.omka@mail.ru>
2023-09-18 13:45:19 +0300

    name

--- git_init.cmd
+++ git_init.cmd
@@ -1,2 +1,8 @@
+echo "# test_java_t1" >> README.md
 git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kot-omka/test_java_t1.git
+git push -u origin main
 pause
--- git_push.cmd
+++ git_push.cmd
@@ -1,11 +1,10 @@
-//gh auth login
 git config user.name "kot-omka"
 git config user.email "kot.omka@mail.ru"
 git config user.name
-git config user.email
-git remote add origin https://github.com/kot-omka/test_java_t1.git
+git config user.emailgit remote set-url origin https://github.com/kot-omka/test_java_t1.git
 git remote -v
 git add .
 git commit -m "name"
+git branch -M main
 git push -u origin main
 pause
