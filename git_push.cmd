4c778858b6ebfe002d34ea810b485c8e0edc0f3a
kot-omka <kot.omka@mail.ru>
2023-09-18 13:12:12 +0300

    name

--- git_push.cmd
+++ git_push.cmd
@@ -3,7 +3,7 @@ git config user.name "kot-omka"
 git config user.email "kot.omka@mail.ru"
 git config user.name
 git config user.email
-git remote add origin https://github.com/kot-omka/kotomka.git
+git remote add origin https://github.com/kot-omka/test_java_t1.git
 git remote -v
 git add .
 git commit -m "name"
